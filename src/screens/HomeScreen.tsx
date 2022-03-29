11a518d8dc96bfab164f116b7ef30694979242bb
ddepu11t <deepanshut@geekyants.com>
2022-03-29 11:38:29 +0530

    feat: list users, scroll to top

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,111 @@
-import { FC, useEffect } from 'react'
-import { Button, Text, View } from 'native-base'
+import { FC, useEffect, useRef, useState } from 'react'
+import {
+  Box,
+  Button,
+  FlatList,
+  IScrollViewProps,
+  ScrollView,
+  Text,
+  View,
+} from 'native-base'
 import { AntDesign, Octicons } from '@expo/vector-icons'
+import { Entypo } from '@expo/vector-icons'
+import User from '../components/User'
+
+type Users = {}
+
+type ScrollViewRef = IScrollViewProps & {
+  scrollTo: (options?: { y: number; x?: number; animated: boolean }) => void
+}
 
 const HomeScreen: FC = () => {
+  const [users, setUsers] = useState<any[]>([])
+  const [results, setResults] = useState(3)
+
+  const [showFilters, setShowFilters] = useState(false)
+
+  const scrollViewRef = useRef<ScrollViewRef>()
+
   useEffect(() => {
     let screenMounted = true
+
+    const fetchUsers = async () => {
+      try {
+        const request = await fetch(`https://randomuser.me/api/?results=${6}`)
+        const data: any = await request.json()
+
+        setUsers((prevState) => {
+          return [...prevState, ...data.results]
+        })
+      } catch (err: any) {
+        console.log(err)
+      }
+    }
+
+    fetchUsers()
 
     return () => {
       screenMounted = false
     }
   }, [])
 
+  function debounce(func: () => void, timeout = 1000) {
+    let timer: NodeJS.Timeout
+
+    return () => {
+      clearTimeout(timer)
+      timer = setTimeout(() => {
+        func()
+      }, timeout)
+    }
+  }
+
   return (
-    <View borderWidth={1} flex={1}>
+    <View flex={0}>
+      <Box height={'100%'}>
+        <ScrollView
+          ref={scrollViewRef}
+          onScrollEndDrag={debounce(() => {
+            const fetchUsers = async () => {
+              try {
+                const request = await fetch(
+                  `https://randomuser.me/api/?results=${results}`
+                )
+
+                const data: any = await request.json()
+
+                setUsers((prevState) => {
+                  return [...prevState, ...data.results]
+                })
+              } catch (err: any) {
+                console.log(err)
+              }
+            }
+
+            fetchUsers()
+
+            console.log('Scroll End')
+          })}
+        >
+          {users &&
+            users.map((item) => {
+              let { email, gender, phone } = item
+
+              return (
+                <User
+                  key={item.dob.date}
+                  email={email}
+                  gender={gender}
+                  mobileNo={phone}
+                  dpURL={item.picture.medium}
+                  fullName={`${item.name.title} ${item.name.first} ${item.name.last}`}
+                  isActive={false}
+                />
+              )
+            })}
+        </ScrollView>
+      </Box>
+
       {/* Scroll To Top Arrow */}
       <Button
         backgroundColor={'#FFFFFF'}
@@ -28,6 +121,9 @@ const HomeScreen: FC = () => {
         shadow={'5'}
         borderWidth={0.5}
         borderColor={'rgba(0,0,0,0.5)'}
+        onPress={() => {
+          scrollViewRef.current?.scrollTo({ y: 0, animated: true })
+        }}
       >
         <AntDesign
           style={{ alignSelf: 'center' }}
@@ -68,6 +164,34 @@ const HomeScreen: FC = () => {
           Filters
         </Text>
       </Button>
+
+      {true && (
+        <View
+          position={'absolute'}
+          bottom={0}
+          width={'100%'}
+          bgColor={'#FFFFFF'}
+        >
+          <Box>
+            <Text
+              fontWeight={700}
+              fontSize={18}
+              lineHeight={21}
+              color={'#555555'}
+              textAlign={'center'}
+            >
+              Filters
+            </Text>
+
+            <Entypo
+              name='cross'
+              size={24}
+              color='black'
+              style={{ position: 'absolute', right: 0 }}
+            />
+          </Box>
+        </View>
+      )}
     </View>
   )
 }
