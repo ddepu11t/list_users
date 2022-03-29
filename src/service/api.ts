726129d02f2194846ae933df7a3a5110e8d8f596
ddepu11t <deepanshut@geekyants.com>
2022-03-29 23:04:30 +0530

    refactore: map through results

--- src/service/api.ts
+++ src/service/api.ts
@@ -6,7 +6,18 @@ const fetchUsers = async (results: number, gender: string) => {
 
     const data: any = await request.json()
 
-    return data.results
+    const newUsers = data.results.map((item: any) => {
+      return {
+        email: item.email,
+        mobileNumber: item.phone,
+        gender: item.gender,
+        dob: item.dob.date,
+        dpURL: item.picture.medium,
+        fullName: `${item.name.title} ${item.name.first} ${item.name.last}`,
+      }
+    })
+
+    return newUsers
   } catch (err: any) {
     console.log(err)
   }
