dfdd2aeb525bc6d81a1f1cc950397a8a3fbd3861
ddepu11t <deepanshut@geekyants.com>
2022-03-29 14:08:15 +0530

    style:  creat spinner component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,19 @@
+import { Heading, HStack, Spinner, Text, View } from 'native-base'
+import { FC } from 'react'
+
+const SpinnerComponent: FC<{ message: string; mt?: number }> = ({
+  message,
+  mt = 16,
+}) => {
+  return (
+    <HStack mt={mt} space={2} justifyContent='center'>
+      <Spinner accessibilityLabel='Loading posts' color={'#5b5b5b'} />
+
+      <Heading color='#5b5b5b' fontSize='lg'>
+        {message}
+      </Heading>
+    </HStack>
+  )
+}
+
+export default SpinnerComponent
