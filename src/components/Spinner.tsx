f07148d7441199ee1d6886519232dfd5788ed75e
ddepu11t <deepanshut@geekyants.com>
2022-03-31 19:04:12 +0530

    style: heading font seight

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -9,7 +9,7 @@ const SpinnerComponent: FC<{ message: string; mt?: number }> = ({
     <HStack mt={mt} space={2} justifyContent='center'>
       <Spinner accessibilityLabel='Loading posts' color={'#5b5b5b'} />
 
-      <Heading color='#5b5b5b' fontSize='lg'>
+      <Heading color='#5b5b5b' fontSize='lg' fontWeight={400}>
         {message}
       </Heading>
     </HStack>
